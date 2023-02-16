cfa6176c5b22ebd4693ad8f971ceeed3dae24b21
Lukas Prochazka <prochazka.lu@gmail.com>
2023-02-16 16:37:31 +0100

    Edit puppeteer

--- index.js
+++ index.js
@@ -3,15 +3,10 @@ import { launch } from 'puppeteer';
 (async () => {
   const browser = await launch();
   const page = await browser.newPage();
-  await page.goto('https://www.wikipedia.org');
+  await page.goto('https://example.com');
 
-  // Get the page's title
   const title = await page.title();
   console.log(`Page title: ${title}`);
-
-  // Get the page's URL
-  const url = await page.url();
-  console.log(`Page URL: ${url}`);
 
   await browser.close();
 })();
