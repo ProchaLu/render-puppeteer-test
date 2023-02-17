e373153f2594ee60d7e17381a7a00882fd6efe49
Lukas Prochazka <prochazka.lu@gmail.com>
2023-02-17 12:19:38 +0100

    Change exec

--- find-chrome.js
+++ find-chrome.js
@@ -1,16 +1,13 @@
 import { exec } from 'child_process';
 
-exec(
-  'find / -type f -name "chromium" -print -quit 2>/dev/null',
-  (err, stdout, stderr) => {
-    if (err) {
-      console.error(`Error running command: ${err}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Command stderr: ${stderr}`);
-      return;
-    }
-    console.log(`Chromium path: ${stdout.trim()}`);
-  },
-);
+exec('which chromium', (err, stdout, stderr) => {
+  if (err) {
+    console.error(`Error running command: ${err}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Command stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Chromium path: ${stdout.trim()}`);
+});
