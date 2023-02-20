afd8d71d9a8526f118731976ea5faf8301f3daeb
Lukas Prochazka <prochazka.lu@gmail.com>
2023-02-20 10:43:15 +0100

    Add executable path

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { launch } from 'puppeteer';
 
 (async () => {
-  const browser = await launch();
+  const browser = await launch({
+    executablePath:
+      '/opt/render/.cache/puppeteer/chrome/linux-1095492/chrome-linux/chrome',
+  });
   const page = await browser.newPage();
   await page.goto('https://example.com');
 
