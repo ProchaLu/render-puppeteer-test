b1fa5f658c24463542f4928764a44339d478723a
Lukas Prochazka <prochazka.lu@gmail.com>
2023-02-22 14:25:45 +0100

    Change path

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 (async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  const page = await browser.newPage({
+    exectuablePath:
+      '/opt/render/project/src/.cache/puppeteer/chrome/linux-1095492/chrome-linux/chrome',
+  });
   await page.goto('https://example.com');
 
   const title = await page.title();
