51fb5078ff474e4871ca098ffda430b2d9794696
Lukas Prochazka <prochazka.lu@gmail.com>
2023-02-22 14:29:24 +0100

    delete path

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 (async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage({
-    exectuablePath:
-      '/opt/render/project/src/.cache/puppeteer/chrome/linux-1095492/chrome-linux/chrome',
-  });
+  const page = await browser.newPage();
   await page.goto('https://example.com');
 
   const title = await page.title();
