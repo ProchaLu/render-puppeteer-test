12bbd9294749d3d9e743dfdcfff611d22434afff
Lukas Prochazka <prochazka.lu@gmail.com>
2023-02-22 13:31:34 +0100

    Add executablePath

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    // Changes the cache location for Puppeteer.
+    // cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+    executablePath: '$HOME/.cache/puppeteer',
+  });
   const page = await browser.newPage();
   await page.goto('https://example.com');
 
