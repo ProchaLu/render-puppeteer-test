ab5568bbb829c56af679fc4fd4128bdaf333c122
Lukas Prochazka <prochazka.lu@gmail.com>
2023-02-23 10:36:10 +0100

    Add logs for puppeteer path

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 console.log('__dirname:', __dirname);
 
+const path = require('path');
+const puppeteerPath = require.resolve('puppeteer');
+const absolutePath = path.resolve(puppeteerPath);
+console.log('Absolute path to Puppeteer:', absolutePath);
+
 // (async () => {
 //   const browser = await puppeteer.launch();
 //   const page = await browser.newPage();
