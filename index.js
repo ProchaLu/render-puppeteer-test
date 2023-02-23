5ab8d9187dcfdddcd995354e8ae6a0a69fc9b008
Lukas Prochazka <prochazka.lu@gmail.com>
2023-02-23 16:51:53 +0100

    Add try catch

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import puppeteer from 'puppeteer';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-console.log('__dirname:', __dirname);
+try {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
 
-fs.readdir(__dirname, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+  const title = await page.title();
+  console.log(`Page title: ${title}`);
 
-  console.log(`Contents of ${__dirname}:`);
-  console.log(files);
-});
-
-fs.readdir(
-  '/opt/render/project/src/.cache/puppeteer/chrome/linux-1095492/chrome-linux/chrome',
-  (err, files) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    console.log(`Contents of puppeteer:`);
-    console.log(files);
-  },
-);
-
-// (async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto('https://example.com');
-
-//   const title = await page.title();
-//   console.log(`Page title: ${title}`);
-
-//   await browser.close();
-// })();
+  await browser.close();
+} catch (err) {
+  console.error(err);
+}
